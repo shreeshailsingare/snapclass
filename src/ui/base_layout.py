4a6c4efdb2330f6b5dafb4af2906d809cd63dcac
Shreeshail Singare <shreeshailsingare5@gmail.com>
2026-05-20 16:06:32 +0530

    fFinishing Touches

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -51,13 +51,13 @@ def style_base_layout():
             .block-container {
              padding-top:1.5rem !important;
             }
-                
+                  
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
                 line-height:1.1 1important;
                 margin-bottom:0rem !important;
-                color: black !important;
+                # color: black !important;
             }
                 
 
