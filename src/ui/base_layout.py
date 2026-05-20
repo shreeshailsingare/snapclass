347144b9db8915708ed422ff2ce22404253fa265
Shreeshail Singare <shreeshailsingare5@gmail.com>
2026-05-20 16:52:41 +0530

    Finishing Touches

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -57,7 +57,6 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 1important;
                 margin-bottom:0rem !important;
-                # color: black !important;
             }
                 
 
@@ -66,7 +65,6 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: black !important;
             }
                 
             h3, h4, p {
