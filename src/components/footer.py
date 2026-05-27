60f5f86e8189deec3b78a93e04d0521f6db146b1
Shreeshail Singare <shreeshailsingare5@gmail.com>
2026-05-27 19:09:23 +0530

    Fixed model error

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,7 +6,7 @@ def footer_home():
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <i style="color:white;">SHREESHAIL SINGARE</i>
+        <i style="color-black;">SHREESHAIL SINGARE</i>
         </div>
                 
                 """, unsafe_allow_html=True)
